c77e9c14fd8c3006a0885d193ed55137e64eff1a
Jaydeep-Ramanuj-tops <jaydeepramanuj.tops@gmail.com>
2024-06-20 17:26:01 +0530

    Module 3 Lecture 1 & 2 20-6

---
+++ Module 3 - C/basic-variables-20-6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main(){
+
+ int b1 = 10;
+  int b2 = 20;
+
+  int add = b1 + b2;
+  int sub = b1 - b2;
+  int mul = b1 * b2;
+  int div = b2 / b1;
+
+printf("Simple arithmetic operations");
+    printf("\nb1 = %d",b1);
+    printf("\nb2 = %d",b2);
+    printf("\nAddition = %d",add);
+    printf("\nSubtraction = %d",sub);
+    printf("\nMultiplication = %d",mul);
+    printf("\nDivision = %d",div);
+
+
+    int temp;
+    scanf("%d",&temp);
+}
---
+++ Module 3 - C/variable-20-6.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void main(){
+
+
+// data_type variable_name = variable_value ;
+
+   int num=103512;
+  int num123=9632;
+  int bhumi=456;
+  int Bhumi = 789;
+  int b1humi=936l;
+  int bhu_mi=7412;
+
+    // int bhu!mi=1236; will give error
+    // int 1bhumi=1236; will give error
+    // int bhu mi=1236; will give error
+    // int int=1236; will give error
+
+
+  // Naming convention
+  // You can't start variable name with number or symbol
+  // You can't use any symbol other than underscore (_)
+  // You can't use keyword as a variable name
+  // You can't use space between variable name
+
+
+
+    printf("%d",bhumi);
+    printf("\n%d",Bhumi);
+    printf("\n%d",b1humi);
+    printf("\n%d",bhu_mi);
+
+
+
+}
