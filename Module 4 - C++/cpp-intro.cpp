57e1d5e7e7b9d30a5462586c56cb7aed64727457
Jaydeep-Ramanuj-tops <jaydeepramanuj.tops@gmail.com>
2024-08-03 17:44:43 +0530

    Module 4 Lecture 1 3-8

---
+++ Module 4 - C++/cpp-intro.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// using namespace std;
+
+int main(){
+    // printf()
+    std::cout<<"Namaste C++";
+
+    int num=10;
+    std::cout<<num;
+
+    int num2;
+    std::cout<<"Enter your number: ";
+    // scanf()
+    std::cin>>num2;
+
+    std::cout<<"Your number is: ",std::cout<<num2;
+
+}
